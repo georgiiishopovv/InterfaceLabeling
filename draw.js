a2a5ed9ca43988df1e3ffabe6f80db06cbd4cd84
georgiiishopovv <131063161+georgiiishopovv@users.noreply.github.com>
2024-07-25 23:16:33 +0200

    Add files via upload

--- draw.js
+++ draw.js
@@ -8,7 +8,6 @@ let shape_drawn = false;
 let currentAnnotation = '';
 let saved = false;
 let xmlAnnotation = '';
-let all_shapes = [];
 
 let text_label = 'Sample Text';
 let text_color = 'black';
@@ -40,6 +39,7 @@ document.getElementById('color').addEventListener('input', function() {
 document.getElementById('width').addEventListener('input', function() {
     shape_width = this.value;
 });
+
 
 document.getElementById('shape-form').addEventListener('change', function(event) {
     shape_type = document.querySelector('input[name="shape"]:checked').value;
@@ -121,115 +121,71 @@ function stopDrawing(event){
             line_width: shape_width,
             text: text_label,
             text_color: text_color,
-            text_size: text_size,
             image_src: images[index]
         };
         annotation_text = currentAnnotation.text;
-        annotations.push(currentAnnotation);
-        currentAnnotation = null;
-
-        // Redraw canvas with updated shapes
-        draw();
-
         drawing = false;
     }
 }
 
-function draw() {
+function draw(event) {
+    if (!drawing) return;
+    const pos = getMousePos(event)
+    const endx = pos.x;
+    const endy = pos.y;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(editedImage, 0, 0);
+    ctx.strokeStyle = shape_color;
+    ctx.lineWidth = shape_width;
+    text_size = `${text_size} Arial`;
+    ctx.font = text_size; 
+    ctx.fillStyle = text_color;
+    if (shape_type === 'rectangle') {
+        ctx.strokeRect(stx, sty, endx - stx, endy - sty);
+        ctx.fillText(text_label, stx, sty - 5);
+    } else if (shape_type === 'circle') {
+        const radius = Math.sqrt(Math.pow(endx - stx, 2) + Math.pow(endy - sty, 2));
+        const angle = Math.atan2(endy - sty, endx - stx);
 
-    // Draw finalized shapes (annotations)
-    annotations.forEach(ann => {
-        ctx.strokeStyle = ann.color;
-        ctx.lineWidth = ann.line_width;
-        ctx.font = `${ann.text_size}px Arial`;
-        ctx.fillStyle = ann.text_color;
+        // Calculate text position on the circle's border
+        const textX = stx + radius * Math.cos(angle);
+        const textY = sty + radius * Math.sin(angle);
 
-        if (ann.type === 'rectangle') {
-            ctx.strokeRect(ann.starting_x, ann.starting_y, ann.end_x - ann.starting_x, ann.end_y - ann.starting_y);
-            ctx.fillText(ann.text, ann.starting_x, ann.starting_y - 5);
-        } else if (ann.type === 'circle') {
-            const radius = Math.sqrt(Math.pow(ann.end_x - ann.starting_x, 2) + Math.pow(ann.end_y - ann.starting_y, 2));
-            const angle = Math.atan2(ann.end_y - ann.starting_y, ann.end_x - ann.starting_x);
-            const textX = ann.starting_x + radius * Math.cos(angle);
-            const textY = ann.starting_y + radius * Math.sin(angle);
-
-            ctx.beginPath();
-            ctx.arc(ann.starting_x, ann.starting_y, radius, 0, 2 * Math.PI);
-            ctx.stroke();
-
-            if (fill) {
-                ctx.fillStyle = ann.color;
-                ctx.fill();
-            }
-
-            ctx.fillText(ann.text, textX, textY);
-        } else if (ann.type === 'connected') {
-            ctx.beginPath();
-            for (let i = 0; i < ann.points.length - 1; i++) {
-                ctx.moveTo(ann.points[i].x, ann.points[i].y);
-                ctx.lineTo(ann.points[i + 1].x, ann.points[i + 1].y);
-            }
-            ctx.stroke();
-            ctx.fillText(ann.text, ann.points[0].x - 20, ann.points[0].y - 10);
-        } else if (ann.type === 'line') {
-            drawLine(ann.starting_x, ann.starting_y, ann.end_x, ann.end_y);
-            ctx.fillText(ann.text, ann.end_x, ann.end_y);
-        }
+        ctx.beginPath();
+        ctx.arc(stx, sty, radius, 0, 2 * Math.PI);
+        ctx.stroke();
 
         if (fill) {
-            ctx.fillStyle = "green";
+            ctx.fillStyle = shape_color;
             ctx.fill();
         }
-        saved = false;
-        shape_drawn = true;
-    });
 
-    // Draw dynamically drawing shape (currentAnnotation)
-    if (drawing && currentAnnotation !== null) {
-        ctx.strokeStyle = currentAnnotation.color;
-        ctx.lineWidth = currentAnnotation.line_width;
-        ctx.font = `${currentAnnotation.text_size}px Arial`;
-        ctx.fillStyle = currentAnnotation.text_color;
-
-        if (currentAnnotation.type === 'rectangle') {
-            ctx.strokeRect(currentAnnotation.starting_x, currentAnnotation.starting_y, currentAnnotation.end_x - currentAnnotation.starting_x, currentAnnotation.end_y - currentAnnotation.starting_y);
-            ctx.fillText(currentAnnotation.text, currentAnnotation.starting_x, currentAnnotation.starting_y - 5);
-        } else if (currentAnnotation.type === 'circle') {
-            const radius = Math.sqrt(Math.pow(currentAnnotation.end_x - currentAnnotation.starting_x, 2) + Math.pow(currentAnnotation.end_y - currentAnnotation.starting_y, 2));
-            const angle = Math.atan2(currentAnnotation.end_y - currentAnnotation.starting_y, currentAnnotation.end_x - currentAnnotation.starting_x);
-            const textX = currentAnnotation.starting_x + radius * Math.cos(angle);
-            const textY = currentAnnotation.starting_y + radius * Math.sin(angle);
-
-            ctx.beginPath();
-            ctx.arc(currentAnnotation.starting_x, currentAnnotation.starting_y, radius, 0, 2 * Math.PI);
-            ctx.stroke();
-
-            if (fill) {
-                ctx.fillStyle = currentAnnotation.color;
-                ctx.fill();
-            }
-
-            ctx.fillText(currentAnnotation.text, textX, textY);
-        } else if (currentAnnotation.type === 'connected') {
-            ctx.beginPath();
-            for (let i = 0; i < points.length - 1; i++) {
-                ctx.moveTo(points[i].x, points[i].y);
-                ctx.lineTo(points[i + 1].x, points[i + 1].y);
-            }
-            ctx.stroke();
-            ctx.fillText(currentAnnotation.text, points[0].x - 20, points[0].y - 10);
-        } else if (currentAnnotation.type === 'line') {
-            drawLine(currentAnnotation.starting_x, currentAnnotation.starting_y, currentAnnotation.end_x, currentAnnotation.end_y);
-            ctx.fillText(currentAnnotation.text, currentAnnotation.end_x, currentAnnotation.end_y);
-        }
-
-        if (fill) {
-            ctx.fillStyle = "green";
-            ctx.fill();
-        }
+        // Draw the text on the circle's border
+        ctx.fillText(text_label, textX, textY);
     }
+    else if (shape_type === 'connected') {
+        points.push({ x: endx, y: endy });
+        ctx.beginPath();
+        for (let i = 0; i < points.length - 1; i++) {
+            ctx.moveTo(points[i].x, points[i].y);
+            ctx.lineTo(points[i + 1].x, points[i + 1].y);
+        }
+        ctx.stroke();
+        points.pop(); // Remove the last point to avoid duplication
+        drawPoints();
+        ctx.fillText(text_label, points[0].x - 20, points[0].y - 10);
+    }
+    else if(shape_type === 'line') {
+        drawLine(stx, sty, endx, endy);
+        ctx.fillText(text_label, endx, endy);
+    }
+    if(fill === 'true')
+    {
+        ctx.fillStyle = "green";
+        ctx.fill();
+    }
+    saved = false;
+    shape_drawn = true;
 }
 
 function drawPoints(){
@@ -283,24 +239,33 @@ function saveAnnotations() {
 
         save_name = `labeled_image${index}`;
         console.log("Saved name", save_name);
-        downloadImage(save_name);
-        all_shapes.push(annotations);
-        annotations = [];
-        console.log("Successful");
-        console.log(all_shapes)
+        saveImageWithMetadata();
     }
 }
 
-function downloadImage(filename) {
-    const dataURL = canvas.toDataURL('image/jpeg');
+function downloadImage(data, file) {
     const a = document.createElement('a');
-    a.href = dataURL;
-    a.download = filename;
+    a.href = data;
+    a.download = file;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-    //runCommand();
+    console.log("Downloaded");
 }
+
+function saveImageWithMetadata(){
+    const dataURL = canvas.toDataURL('image/jpeg');
+    const exifObj = piexif.load(dataURL);
+
+    exifObj['0th'][piexif.ImageIFD.UserComment] = piexif.sanitizeUserComment(xmlAnnotation);
+
+    const exifBytes = piexif.dump(exifObj);
+    const newDataURL = piexif.insert(exifBytes, dataURL);
+
+    console.log("Preparing download", xmlAnnotation);
+    downloadImage(newDataURL, save_name);
+}
+
 
 function drawLine(x1, y1, x2, y2) {
     lines.push({x1: x1, y1: y1, x2: x2, y2:y2});
@@ -331,21 +296,8 @@ document.querySelector('.arrow-right-edit').addEventListener('click', function()
             canvasImage(index);
             //currentImageIndex = (currentImageIndex + 1) % images.length;
         }
-    }cd
+    }
 });  
-
-// async function runCommand() {
-//     const command = '-ls -l';
-//     const response = await fetch(`http://localhost:3000//runcmd?command=${encodeURIComponent(command)}`);
-//     if (response.ok) {
-//         const result = await response.text();
-//         console.log('Command output:', result);
-//         alert('Command output:\n' + result);
-//     } else {
-//         console.error('Failed to execute command:', response.statusText);
-//         alert('Failed to execute command:\n' + response.statusText);
-//     }
-// }
 
 
 
